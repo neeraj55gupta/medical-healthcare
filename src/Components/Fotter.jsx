8e5e72c66327d2171cb152e690f1b10ed9428f4c
neeraj55gupta <neeraj55gupta@gmail.com>
2024-08-08 11:56:45 +0530

    first commit

---
+++ src/Components/Fotter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Fotter.css'
+export default function Fotter() {
+  return (
+    <footer className='fotter'>
+    <h2>Fotter</h2>
+    <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, harum modi consequatur soluta animi quas laudantium saepe temporibus. Corporis rem explicabo tempora ipsum eveniet quam facere deleniti a maxime laborum!
+    </p>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi asperiores eveniet ea omnis, laborum voluptatibus aspernatur fugiat eum similique explicabo corrupti quaerat blanditiis maiores repudiandae assumenda reprehenderit facere. Voluptate, ab.
+    </p>
+    <p> 
+        2024 your Company Name.All Rights reserved.
+    </p>
+    </footer>
+  )
+}
+
+
+
+
